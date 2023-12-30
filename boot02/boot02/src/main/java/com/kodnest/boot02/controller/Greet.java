8ef4735c94d7557b39e7db943e439aebc8f41733
ARJUN TELI <modiarjunsahu1161999@gmail.com>
2023-12-30 23:12:43 +0530

    Basic Projects

---
+++ boot02/boot02/src/main/java/com/kodnest/boot02/controller/Greet.java
@@ -0,0 +1,13 @@
+package com.kodnest.boot02.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Greet {
+	@GetMapping(  "/greet")
+	public String greet() {
+		return "Hi Namaste!!!!";
+	}
+
+}
